06004eedf95daa9c053f5c51635ebbf6f575aabc
Ruke <chengy19930827@gmail.com>
2017-03-05 20:29:10 +0800

    添加一些配置和模板文件布局

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controlle;
-
+use Illuminate\Support\Facades\DB;
 class indexController extends Controller{
-
-	public function __construct(){
-		view()->share('netTitle','FUNCTION');
-	}
 
 	/**
 	 * 项目首页
 	 */
 	public function index(){
+		
 		return view('index/index',array('title'=>'主页'));
 	}
 }
