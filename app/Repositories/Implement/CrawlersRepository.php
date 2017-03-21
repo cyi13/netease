9818caf433605caaccdc7e8f7ea2a459e5395fa1
ruke <chengy19930827@gmail.com>
2017-03-21 18:16:30 +0800

    修改bug

--- app/Repositories/Implement/CrawlersRepository.php
+++ app/Repositories/Implement/CrawlersRepository.php
@@ -256,23 +256,20 @@ class CrawlersRepository extends Common implements CrawlersInterface{
             //创建人空间链接
             $spaceLinkList = $pageList[6];
         }
-        
-        $playlist = array();
+
+        $playList = array();
         if(!empty($listId)){
             //数据先存储到数组中统一插入
             foreach ($listId as $k => $v) {
-                //用redis来判断重复
-                if($this->Redis->sadd('playlistId',$v)){
-                    $data = array('listId'       => $v,
-                                'listTitle'    => $titleList[$k],
-                                'listImg'      => $imgList[$k],
-                                'link'         => self::COLUDDMIAN.$hrefList[$k],
-                                'listenNum'    => $this->chineseToNumber($listenList[$k]),
-                                'by'           => $byList[$k],
-                                'spaceLink'    => self::COLUDDMIAN.$spaceLinkList[$k]);
-                    //存放到数组里面 统一插入
-                    $playList[$v] = $data;
-                }
+                $data = array('listId'     => $v,
+                            'listTitle'    => $titleList[$k],
+                            'listImg'      => $imgList[$k],
+                            'link'         => self::COLUDDMIAN.$hrefList[$k],
+                            'listenNum'    => $this->chineseToNumber($listenList[$k]),
+                            'by'           => $byList[$k],
+                            'spaceLink'    => self::COLUDDMIAN.$spaceLinkList[$k]);
+                //存放到数组里面 统一插入
+                $playList[$v] = $data;
             }
         }
         return $playList;
@@ -421,7 +418,7 @@ class CrawlersRepository extends Common implements CrawlersInterface{
             //先判断是否有重复的数据
             $idList = array_keys($array);
             $idListString = implode(',',$idList);
-            $res = $Model->select($idColumn)->whereIn($idColumn,$idListString)->get();
+            $res = $Model->select($idColumn)->whereIn($idColumn,$idListString)->get()->toArray();
             if(!empty($res)){
                 foreach($res as $value){
                     $id = $value[$idColumn];
